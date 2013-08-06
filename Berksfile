f67388710e6d5f565c2a417a097752374929bf76
vvakame <vvakame+dev@gmail.com>
2013-08-06 17:16:17 +0900

    Jenkinsのレシピが未だにHudsonベースっぽい所があったのでgitから最新版を利用するように変更

--- Berksfile
+++ Berksfile
@@ -1,7 +1,7 @@
 site :opscode
 
 cookbook 'java'
-cookbook 'jenkins'
+cookbook 'jenkins', git: 'https://github.com/opscode-cookbooks/jenkins.git', ref: '0ff2a4e20e5ca0646a112b3bc11c22af87a9f3ad'
 
 cookbook 'iptables'
 
--- roles/jenkins.rb
+++ roles/jenkins.rb
@@ -1,16 +1,21 @@
 name "jenkins"
 description "jenkins server"
 
-run_list "recipe[iptables]", "recipe[java]", "recipe[jenkins]"
+run_list "recipe[iptables]", "recipe[java]", "recipe[jenkins::server]", "recipe[jenkins::iptables]"
 
 override_attributes "java" => {
 	"install_flavor" => "oracle",
-	"jdk_version" => 7,
+	"jdk_version" => "7",
 	"oracle" => {
 		"accept_oracle_download_terms" => true
 	}
 }
 
 override_attributes "jenkins" => {
-	"iptables_allow" => "enable"
+	"iptables_allow" => "enable",
+	"server" => {
+		"plugins" => [
+			"git"
+		]
+	}
 }
