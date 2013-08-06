2229bfc27ab88e4ec026f0234c387551ae5336bf
vvakame <vvakame+dev@gmail.com>
2013-08-06 20:56:56 +0900

    ReVIEW用のレシピをざくーっと書いた(まだ動いてない

--- site-cookbooks/review/recipes/default.rb
+++ site-cookbooks/review/recipes/default.rb
@@ -2,7 +2,42 @@
 # Cookbook Name:: review
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, vvakame
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# testing on only CentOS 6.4
+# currently under testing
+
+include_recipe "yum::epel"
+
+# for platex
+yum_package "texlive-latex" do
+	action :install
+end
+
+yum_package "texlive-east-asian" do
+	action :install
+end
+
+yum_package "xdvik" do
+	action :install
+end
+
+yum_package "dvipdfmx" do
+	action :install
+end
+
+yum_package "ipa-mincho-fonts" do
+	action :install
+end
+
+yum_package "ipa-gothic-fonts" do
+	action :install
+end
+
+# for review
+gem_package "review" do
+	action :install
+end
