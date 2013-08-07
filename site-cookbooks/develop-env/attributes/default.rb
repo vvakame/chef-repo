21273e52cf6d4c42f0350d1feca0f41a9afb11d2
vvakame <vvakame+dev@gmail.com>
2013-08-07 16:17:26 +0900

    develop-env を更新して git と grunt-cli を入れるように変更

---
+++ site-cookbooks/develop-env/attributes/default.rb
@@ -0,0 +1,11 @@
+include_attribute "nodejs"
+include_attribute "npm"
+
+# レシピで更新した分は他に影響を及ぼさないって書いてあった
+# http://www.engineyard.co.jp/blog/2013/breaking_changes_chef_11/
+node.override['nodejs']['src_url'] = "http://nodejs.org/dist" # 何故か指定しないと上手く動かない気がする？？？
+node.override['nodejs']['install_method'] = "binary"
+node.override['nodejs']['version'] = "0.10.15"
+node.override['nodejs']['checksum_linux_x64'] = "0b5191748a91b1c49947fef6b143f3e5e5633c9381a31aaa467e7c80efafb6e9"
+
+node.override['npm']['version'] = "1.3.7"
--- site-cookbooks/develop-env/recipes/default.rb
+++ site-cookbooks/develop-env/recipes/default.rb
@@ -2,7 +2,23 @@
 # Cookbook Name:: develop-env
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, vvakame
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "git"
+
+def npm_missing?
+	! run_context.loaded_recipe?("npm")
+end
+
+if npm_missing?
+	Chef::Log.error("npm cookbook is missing. Please add to the run_list.")
+	raise "required npm" # TODO 失敗させかたこれで正しいのか謎
+end
+
+npm_package "grunt-cli" do
+	version "0.1.9"
+	action :install
+end 
