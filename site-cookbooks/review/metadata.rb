c01bdd41d5bff1cf9c5989a24de05f742a7b2925
vvakame <vvakame+dev@gmail.com>
2013-08-08 21:53:05 +0900

    書き方間違えたり消しちゃいけないの消したりしてたのを直した

--- site-cookbooks/review/metadata.rb
+++ site-cookbooks/review/metadata.rb
@@ -6,4 +6,5 @@ description      'Installs/Configures review'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "texlive2013", "jenkins", ""
+depends "texlive2013"
+depends "jenkins"
