3e049e4ac18707485b8fe39f3cd536e7d7cfe038
vvakame <vvakame+dev@gmail.com>
2013-08-08 17:00:08 +0900

    review-sample-book をビルドするJenkinsジョブを自動で追加できるようにした

--- site-cookbooks/review/attributes/default.rb
+++ site-cookbooks/review/attributes/default.rb
@@ -1,4 +1,14 @@
 include_attribute "texlive2013"
+include_attribute "jenkins"
 
 # too heavy on platform_family eq rhel
 default['review']['pdf'] = false
+
+# add jenkins tasks
+default['review']['jenkins']['sample'] = false
+default['review']['jenkins']['job']['name'] = "ReVIEW-sample-book"
+default['review']['jenkins']['job']['git']['repository'] = "https://github.com/takahashim/review-sample-book.git"
+default['review']['jenkins']['job']['git']['branch'] = "**"
+default['review']['jenkins']['job']['git']['trigger_spec'] = "H/5 * * * *"
+default['review']['jenkins']['job']['src_folder'] = "./src"
+default['review']['jenkins']['job']['result_exclude'] = "src/book/"
--- site-cookbooks/review/recipes/default.rb
+++ site-cookbooks/review/recipes/default.rb
@@ -31,3 +31,45 @@ end
 gem_package "review" do
 	action :install
 end
+
+# for review-epubmaker
+package "zip" do
+	action :install
+end
+
+# jenkins job setup
+if node['review']['jenkins']['sample']
+	job_name = "#{node['review']['jenkins']['job']['name']}"
+	git_repository = "#{node['review']['jenkins']['job']['git']['repository']}"
+	git_branch = "#{node['review']['jenkins']['job']['git']['branch']}"
+	git_trigger_spec = "#{node['review']['jenkins']['job']['git']['trigger_spec']}"
+	src_folder = "#{node['review']['jenkins']['job']['src_folder']}"
+	result_exclude = "#{node['review']['jenkins']['job']['result_exclude']}"
+
+	job_config = File.join(node['jenkins']['server']['home'], "#{job_name}-config.xml")
+
+	jenkins_job job_name do
+		action :nothing
+		config job_config
+	end
+
+	template job_config do
+		source "jenkins-config-review.xml.erb"
+		owner node['jenkins']['server']['group']
+		group node['jenkins']['server']['user']
+		mode 00644
+		variables(
+			:git_repository => git_repository,
+			:git_branch => git_branch,
+			:trigger_spec => git_trigger_spec,
+			:src_folder => src_folder,
+			:result_exclude => result_exclude,
+			:job_name => job_name
+		)
+		notifies :update, resources(:jenkins_job => job_name), :immediately
+		notifies :build, resources(:jenkins_job => job_name), :immediately
+		not_if do
+			File.exists?(job_config)
+		end
+	end
+end
